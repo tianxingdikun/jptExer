fc2c298f0cde30a98bf95aeeb1fe3e7b365faa4e
zhangzhao <zhanglinzhao@gwssi.com.cn>
2020-06-01 21:00:19 +0800

    本地存储threadLocal

---
+++ src/main/java/Thread/jpt213/ThreadLocalVariableHolder.java
@@ -0,0 +1,47 @@
+package Thread.jpt213;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadLocalVariableHolder {
+    private static ThreadLocal<Integer> value =
+            new ThreadLocal<Integer>() {
+                private Random rand = new Random(47);
+                protected synchronized Integer initialValue() {
+                    return rand.nextInt(10000);
+                }
+            };
+    public static void increment() {
+        value.set(value.get() + 1);
+    }
+    public static int get() { return value.get(); }
+
+    //本地存储
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0; i < 5; i++)
+            exec.execute(new Accessor(i));
+        TimeUnit.SECONDS.sleep(3);  // Run for a while
+        exec.shutdownNow();         // All Accessors will quit
+    }
+
+
+}
+
+class Accessor implements Runnable {
+    private final int id;
+    public Accessor(int idn) { id = idn; }
+    public void run() {
+        while(!Thread.currentThread().isInterrupted()) {
+            ThreadLocalVariableHolder.increment();
+            System.out.println(this);
+            Thread.yield();
+        }
+    }
+    public String toString() {
+        return "#" + id + ": " +
+                ThreadLocalVariableHolder.get();
+    }
+}
