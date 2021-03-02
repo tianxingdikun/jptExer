a7093e2ea893a35cebbd22856d1fd996f400fa4f
zhangzhao <zhanglinzhao@gwssi.com.cn>
2021-03-02 17:33:53 +0800

    对象bo

---
+++ src/main/java/bo/CollegeStudentBO.java
@@ -0,0 +1,14 @@
+package bo;
+
+public class CollegeStudentBO extends StudentBO {
+
+    private int collegeScore;
+
+    public int getCollegeScore() {
+        return collegeScore;
+    }
+
+    public void setCollegeScore(int collegeScore) {
+        this.collegeScore = collegeScore;
+    }
+}
---
+++ src/main/java/bo/StudentBO.java
@@ -0,0 +1,32 @@
+package bo;
+
+public class StudentBO {
+
+    private String stuName;
+    private String stuClass;
+    private int age;
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public String getStuClass() {
+        return stuClass;
+    }
+
+    public void setStuClass(String stuClass) {
+        this.stuClass = stuClass;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
