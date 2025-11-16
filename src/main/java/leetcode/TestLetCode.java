b0229fdb24304824d401108cb5029e849b27f8ad
zhanglinzhao <zhanglinzhao@58.com>
2025-11-16 11:33:17 +0800

    双指针最大容积

--- src/main/java/leetcode/TestLetCode.java
+++ src/main/java/leetcode/TestLetCode.java
@@ -139,6 +139,27 @@ public class TestLetCode {
         return res;
     }
 
+    /**
+     * @param height
+     * @Description: 盛最多水的容器
+     */
+    public int maxArea(int[] height) {
+        if (null == height || height.length == 0) return 0;
+        int left = 0;
+        int right = height.length - 1;
+        int res = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            res = Math.max(res, area);
+            if (height[right] < height[left]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
+    }
+
 
     public static void main(String[] args) {
         TestLetCode letCode = new TestLetCode();
